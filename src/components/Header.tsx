7f87239708f52f6fbd6b2a2d30f794cacf960808
aniele <ardissonaniele99@gmail.com>
2025-08-27 15:48:20 -0400

    feat: Implementa responsividade no Header e Hero

---
+++ src/components/Header.tsx
@@ -0,0 +1,153 @@
+import {
+  ChevronDownIcon,
+  Bars3Icon,
+  XMarkIcon,
+} from "@heroicons/react/20/solid";
+import { useState } from "react";
+import "./header.css";
+import logo from "../assets/images/LOGO_BRANCO.png";
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [openSubmenus, setOpenSubmenus] = useState<Record<string, boolean>>({});
+
+  const toggleSubmenu = (menuName: string) => {
+    setOpenSubmenus((prevState) => ({
+      ...prevState,
+      [menuName]: !prevState[menuName],
+    }));
+  };
+
+  return (
+    <header>
+      <div className='container'>
+        <div className='logo'>
+          <img src={logo} alt='Logo Cacauron' />
+          <p>Cacauron</p>
+        </div>
+        <button
+          className={`mobile-menu-btn ${isMenuOpen ? "open" : ""}`}
+          onClick={() => {
+            setIsMenuOpen(!isMenuOpen);
+            setOpenSubmenus({});
+          }}
+          aria-label='Toggle menu'
+        >
+          {isMenuOpen ? <XMarkIcon /> : <Bars3Icon />}
+        </button>
+        <nav className={isMenuOpen ? "nav-open" : ""}>
+          {isMenuOpen && (
+            <div className='search mobile-search'>
+              <input type='text' placeholder='Pesquisar...' />
+              <i className='fas fa-search'></i>
+            </div>
+          )}
+          <ul>
+            <li>
+              <a href='/'>Início</a>
+            </li>
+            <li className={openSubmenus["institucional"] ? "open" : ""}>
+              <a
+                href='/institucional'
+                aria-haspopup='true'
+                onClick={(e) => {
+                  e.preventDefault();
+                  toggleSubmenu("institucional");
+                }}
+              >
+                Institucional
+                <ChevronDownIcon
+                  style={{
+                    transform: openSubmenus["institucional"]
+                      ? "rotate(180deg)"
+                      : "rotate(0deg)",
+                  }}
+                />
+              </a>
+              <ul>
+                <li>
+                  <a href='/institucional/diretoria'>Diretoria</a>
+                </li>
+                <li>
+                  <a href='/institucional/historia'>História</a>
+                </li>
+                <li>
+                  <a href='/institucional/missao-e-valores'>Missão e Valores</a>
+                </li>
+              </ul>
+            </li>
+            <li className={openSubmenus["imprensa"] ? "open" : ""}>
+              <a
+                href='/imprensa'
+                aria-haspopup='true'
+                onClick={(e) => {
+                  e.preventDefault();
+                  toggleSubmenu("imprensa");
+                }}
+              >
+                Imprensa
+                <ChevronDownIcon
+                  style={{
+                    transform: openSubmenus["imprensa"]
+                      ? "rotate(180deg)"
+                      : "rotate(0deg)",
+                  }}
+                />
+              </a>
+              <ul>
+                <li>
+                  <a href='/imprensa/noticias'>Notícias</a>
+                </li>
+                <li>
+                  <a href='/imprensa/artigos'>Artigos</a>
+                </li>
+                <li>
+                  <a href='/imprensa/revistas'>Revistas</a>
+                </li>
+              </ul>
+            </li>
+            <li className={openSubmenus["eventos"] ? "open" : ""}>
+              <a
+                href='/eventos'
+                aria-haspopup='true'
+                onClick={(e) => {
+                  e.preventDefault();
+                  toggleSubmenu("eventos");
+                }}
+              >
+                Eventos
+                <ChevronDownIcon
+                  style={{
+                    transform: openSubmenus["eventos"]
+                      ? "rotate(180deg)"
+                      : "rotate(0deg)",
+                  }}
+                />
+              </a>
+              <ul>
+                <li>
+                  <a href='/eventos/agenda'>Agenda de Eventos</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href='/publicacoes'>Publicações</a>
+            </li>
+            <li>
+              <a href='/convenios'>Convênios</a>
+            </li>
+            <li>
+              <a href='/contato'>Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className='search desktop-search'>
+        <input type='text' placeholder='Pesquisar...' />
+        <i className='fas fa-search'></i>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import "./hero.css";
+
+function Hero() {
+  return (
+    <section id='hero'>
+      <div>
+        <h1 className='hero-title'>
+          CACAURON: Cultivando o Futuro do Cacau em Rondônia
+        </h1>
+        <p>
+          A CACAURON é a união de produtores e chocolateiros que, juntos,
+          cultivam a excelência e a tradição. Nosso compromisso é com a
+          sustentabilidade e a qualidade, garantindo que o selo "Rondônia Cacau"
+          seja a sua garantia de uma experiência inesquecível e genuína.
+        </p>
+        <a href='' className='hero-button'>
+          Conheça Nosso Trabalho
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
