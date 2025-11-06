133b2231e0fb54aad864f48cba91f9e78a6223d3
aniele <ardissonaniele99@gmail.com>
2025-11-06 10:56:22 -0400

    refactor(ui): melhora performance da imagem da capa e corrige espaçamento na seção OurStory

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,22 @@
-import "./hero.css"; 
-import { useTranslation } from 'react-i18next'; 
-
+import "./hero.css";
+import { useTranslation } from "react-i18next";
+import capa from "../assets/images/FOTO---CACAU.webp";
 
 function Hero() {
   const { t } = useTranslation();
 
   return (
     <section id='hero'>
+      <img
+        src={capa}
+        alt={t("hero.imageAlt")}
+        className='imagem-de-fundo'
+      ></img>
       <div>
-        <h1 className='hero-title'>{t('hero.title')}</h1>
-        <p>{t('hero.description')}</p>
+        <h1 className='hero-title'>{t("hero.title")}</h1>
+        <p>{t("hero.description")}</p>
         <a href='#our-story' className='hero-button'>
-          {t('hero.buttonText')}
+          {t("hero.buttonText")}
         </a>
       </div>
     </section>
